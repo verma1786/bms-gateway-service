fc9ecab247ae4a0dcbac4f9b5974227c39350cdb
vinay <vinay.verma107>
2022-09-13 15:21:23 +0530

    Initial Commit

---
+++ src/test/java/com/gateway/bms/BmsGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.bms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmsGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
