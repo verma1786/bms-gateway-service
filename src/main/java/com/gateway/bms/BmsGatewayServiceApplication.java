f7cbd8e9a3b4990b7ba67a0b7546ab0f887dd7f5
vinay <vinay.verma107>
2022-09-13 17:30:24 +0530

    Initial Commit

--- src/main/java/com/gateway/bms/BmsGatewayServiceApplication.java
+++ src/main/java/com/gateway/bms/BmsGatewayServiceApplication.java
@@ -2,8 +2,12 @@ package com.gateway.bms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableHystrix
 public class BmsGatewayServiceApplication {
 
 	public static void main(String[] args) {
